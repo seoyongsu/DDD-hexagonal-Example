501ac6ccc0502a7d9788ee88a348352e7cab0056
seoyongsu <sdying03@naver.com>
2024-02-08 16:10:00 +0900

    #2. Base Domain Interface 추가

---
+++ src/main/java/org/example/hexagonalexample/domain/core/AggregateRoot.java
@@ -0,0 +1,16 @@
+package org.example.hexagonalexample.domain.core;
+
+
+/**
+ * Base Aggregate Root
+ * <pre>
+ *     aggregate implement this!
+ * </pre>
+ *
+ * @author seoyongsu
+ */
+
+public interface AggregateRoot<ID extends Identity> extends Entity<ID> {
+
+
+}
---
+++ src/main/java/org/example/hexagonalexample/domain/core/Event.java
@@ -0,0 +1,12 @@
+package org.example.hexagonalexample.domain.core;
+
+/**
+ * Base interface for Events
+ *  <pre>
+ *      Event implement this!
+ *  </pre>
+ *
+ * @author seoyongsu
+ */
+public interface Event {
+}
