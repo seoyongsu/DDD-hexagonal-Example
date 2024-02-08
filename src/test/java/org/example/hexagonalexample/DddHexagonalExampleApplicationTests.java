ff58b4db20a31a9db562453a6daecab7f9223b33
seoyongsu <sdying03@naver.com>
2024-02-08 16:00:00 +0900

    #1 프로 젝트 기본 세팅

---
+++ src/test/java/org/example/hexagonalexample/DddHexagonalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hexagonalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddHexagonalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
