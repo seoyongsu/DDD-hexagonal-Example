0aea5a34ebd67b8470efd8c0b4818307571faed2
seoyongsu <sdying03@naver.com>
2024-08-02 16:17:01 +0900

    #2. Domain 영역 모듈 분리

--- src/main/java/org/example/hexagonalexample/domain/order/event/OrderPaidEvent.java
+++ src/main/java/org/example/hexagonalexample/domain/order/event/OrderPaidEvent.java
@@ -1,7 +1,7 @@
-package org.example.hexagonalexample.domain.order.event;
+package org.example.order.domain.event;
 
 
-import org.example.hexagonalexample.domain.order.entity.Order;
+import org.example.order.domain.entity.Order;
 
 /**
  * 주문 결제 이벤트
