ae3720ba97c98eb82303da102e76522b0b746eb0
seoyongsu <sdying03@naver.com>
2024-10-04 10:48:35 +0900

    #8. 기존 단일 프로젝트 내용 제거

--- src/main/java/org/example/hexagonalexample/application/ports/out/repository/OrderCommandRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.hexagonalexample.application.ports.out.repository;
-
-import org.example.hexagonalexample.domain.order.entity.Order;
-
-public interface OrderCommandRepository {
-    Order save(Order order);
-}
--- src/main/java/org/example/hexagonalexample/infrastructure/persistence/jpa/OrderTableRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.hexagonalexample.infrastructure.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderTableRepository extends JpaRepository<OrderTable, String> {
-}
